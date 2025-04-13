9e0c30eaeb1dc9dcdf6795de91de426b862d8f38
Ginevra Cepparulo <cepparuloginevra@gmail.com>
2025-04-13 01:50:52 +0100

    Datura import

--- backend/PredictionVerifier.py
+++ backend/PredictionVerifier.py
@@ -3,9 +3,7 @@ from typing import List, Dict
 import requests
 import re
 import os 
-from datura import Datura
-
-
+from datura_py import Datura
 
 # Initialise environment variables
 MODEL_NAME = os.environ.get("MODEL_NAME", "gpt-4o-2024-08-06")
