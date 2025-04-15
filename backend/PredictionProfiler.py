9d25a29091112408be05f4f3c6c995cbe2a469cb
Ginevra Cepparulo <cepparuloginevra@gmail.com>
2025-04-15 15:17:22 +0200

    New profile builder datura api

--- backend/PredictionProfiler.py
+++ backend/PredictionProfiler.py
@@ -30,17 +30,8 @@ class PredictionProfiler:
         }
         
         params = {
-            "query": f"from:{handle}",
-            "sort": "Top",
-            "lang": "en",
-            "verified": True,
-            "blue_verified": True,
-            "is_quote": False,
-            "is_video": False,
-            "is_image": False,
-            "min_retweets": 0,
-            "min_replies": 0,
-            "min_likes": 0,
+            "query": "until:2024-12-31",
+            "user": handle,
             "count": 30  #100
         }
         
